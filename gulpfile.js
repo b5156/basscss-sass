418ccbf31deffb516e3a8087c2726caab159bae9
good318 <304469323@qq.com>
2018-01-15 10:52:55 +0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,17 @@ const sass = require('gulp-ruby-sass');
 const rename =require('gulp-rename');
 gulp.task('default', function () {
         return sass('scss/basscss.scss')
+
             .on('error', sass.logError)
+            .pipe(postcss([ autoprefixer({browsers: ['iOS 7', 'Android >= 4.0', '> 1%']}) ]))
             .pipe(gulp.dest('dist'))
+
+    }
+);
+
+gulp.task('wxss', function () {
+        return sass('scss/basscss.scss')
+            .on('error', sass.logError)
             .pipe(rename({extname :'.wxss'}))
             .pipe(gulp.dest('dist'))
 
