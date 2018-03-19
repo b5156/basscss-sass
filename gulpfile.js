6bb6f05d23de0cc139208ed6adfba77aae749b00
good318 <304469323@qq.com>
2018-03-19 10:57:54 +0800

    add ie10 browserslist

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-ruby-sass');
 const rename =require('gulp-rename');
+const postcss=require('gulp-postcss');
+const autoprefixer=require('autoprefixer');
 gulp.task('default', function () {
         return sass('scss/basscss.scss')
 
             .on('error', sass.logError)
-            .pipe(postcss([ autoprefixer({browsers: ['iOS 7', 'Android >= 4.0', '> 1%']}) ]))
+            .pipe(postcss([ autoprefixer({browsers: ['iOS 7', 'Android >= 4.0', '> 1%','IE 10']}) ]))
             .pipe(gulp.dest('dist'))
 
     }
@@ -15,6 +17,16 @@ gulp.task('wxss', function () {
         return sass('scss/basscss.scss')
             .on('error', sass.logError)
             .pipe(rename({extname :'.wxss'}))
+            .pipe(gulp.dest('dist'))
+
+    }
+);
+
+
+gulp.task('test', function () {
+        return sass('test.scss')
+            .on('error', sass.logError)
+
             .pipe(gulp.dest('dist'))
 
     }
